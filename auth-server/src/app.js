7360c9ffba28230284e406048b73d752c16fb912
root <root@STDLTUC-L018.localdomain>
2020-06-08 18:20:41 +0300

    Fix README

--- auth-server/src/app.js
+++ auth-server/src/app.js
@@ -1,24 +1,16 @@
 'use strict';
-
-// 3rd Party Resources
 const express = require('express');
 const cors = require('cors');
-const path=require('path');
-// Esoteric Resources
-// const oauth = require('./github.js');
-const linkedinOuth = require('./linkedln.js');
-// Prepare the express app
+const path = require('path');
+const linkedinOuth = require('./linkedln-Middleware.js');
 const app = express();
-
-// App Level MW
 app.use(cors());
-
-// Website Files
 app.use(express.static('./public'));
-app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'../public/index.html'));
-});
 // Routes
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 app.get('/oauth', linkedinOuth, (req, res) => {
   res.status(200).json({ user: req.user, token: req.token });
 });
